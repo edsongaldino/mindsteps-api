a8740253ab35ed9e70c0b6501afc12bcc40ecf4f
Edson Galdino De Araujo <45825@tjmt.jus.br>
2025-08-28 15:35:02 -0400

    Alteração pra criar senha ao cadastrar usuário

--- MindSteps.Application/DTOs/UsuarioDto.cs
+++ MindSteps.Application/DTOs/UsuarioDto.cs
@@ -5,5 +5,6 @@ public class UsuarioDto
     public Guid Id { get; set; }
     public string Nome { get; set; }
     public string Email { get; set; }
-    public string Telefone { get; set; }
+    public string Senha { get; set; }
+	public string Telefone { get; set; }
 }
